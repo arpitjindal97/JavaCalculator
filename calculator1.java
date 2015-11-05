cf030ad4735d6b736ddd120893e9f65e15423891
arpitjindal97 <arpitjindal1997@hotmail.com>
2015-11-06 00:02:09 +0530

    Create calculator1.java

---
+++ calculator1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+class calculator1
+{
+    public static void main(String args[])throws IOException,Exception
+    {
+        InputStreamReader rd=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(rd);
+        System.out.print('\f');
+        System.out.println("Enter the expression");
+        String str=br.readLine(),st="";
+        do
+        {  
+            str=str+st;
+            if(!st.equals(""))
+            {
+                System.out.print('\f');
+                System.out.println(str);
+            }
+            Calculate t=new Calculate(str);
+            str=t.getString();
+            if(str.equals("Syntax Error")||str.equals("inf"))
+            {
+                System.out.println(str);
+                return;
+            }
+            else
+                System.out.print(str);
+            st=br.readLine();
+            if(st.length()==0)
+                return;
+        }while(st.length()!=0);
+    }
+}

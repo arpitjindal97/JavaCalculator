5d3b7ce02886dc8abb8b0d2b023dbb1e48137558
arpitjindal97 <arpitjindal1997@hotmail.com>
2015-11-06 00:03:48 +0530

    Update and rename test.java to GUICalc.java

--- GUICalc.java
+++ GUICalc.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
-public class test implements ActionListener
+public class GUICalc implements ActionListener
 {
     JLabel answer;
     JTextField tf;
